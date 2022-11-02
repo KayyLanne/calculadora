40568e1e118464d1e034939e05339e825926ef6e
github_kayylanne <kaylanne.regina@hotmail.com>
2022-11-02 00:44:50 -0300

    divisão de frames adicionadas

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 
+
+#cores
+
+cor1 = "#1e1f1e" #preta
+
 janela = Tk()
 janela.title("Calculadora")
-janela.geometry("300x400")
+janela.geometry("235x318")
+janela.config(bg=cor1)
+
+frame_tela = Frame(janela, width=235, height=50)
+frame_tela.grid(row=0, column=0)
 
 janela.mainloop()
 
